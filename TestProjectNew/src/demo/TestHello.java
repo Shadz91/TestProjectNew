b0feaca0fe755beaabdcff7eddc24a9e9bb15fa9
Shadab <Shadab@DELL>
2018-11-14 22:53:55 +0530

    Added System.out.println("My passion is Coding")

--- TestProjectNew/src/demo/TestHello.java
+++ TestProjectNew/src/demo/TestHello.java
@@ -4,7 +4,7 @@ public class TestHello {
 
 	public static void main(String[] args) {
 		System.out.println("My Name is Shadab Ahmed");
-		System.out.println("SDET");
+		System.out.println("My passion is Coding");
 	}
 
 }
